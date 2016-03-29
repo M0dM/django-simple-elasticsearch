a4b37650a2101668678bdda4281484e99d6ef174
Benoit Brayer <brayer.benoit@gmail.com>
2016-03-29 13:03:12 +0200

    - add_blog & add_post views - flake8 config to ignore migrations - first migration

--- examples/example-simple/blog/urls.py
+++ examples/example-simple/blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, add_blog, add_post
 
 urlpatterns = [
-  url(r'^$', index, name='index'),
+    url(r'^$', index, name='index'),
+    url(r'^blog/add$', add_blog, name='add_blog'),
+    url(r'^post/add$', add_post, name='add_post'),
 ]
--- examples/example-simple/blog/views.py
+++ examples/example-simple/blog/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.utils.translation import ugettext_lazy as _
+from .forms import BlogForm, BlogPostForm
 
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def add_blog(request):
+    form = BlogForm(data=request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            _('SUCCESS, blog saved.')
+        )
+        return redirect('blog:index')
+    else:
+        context = {
+            'form': form,
+        }
+        return render(request, 'blog/add_post.html', context)
+
+
+def add_post(request):
+    form = BlogPostForm(data=request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            _('SUCCESS, post saved.')
+        )
+        return redirect('blog:index')
+    else:
+        context = {
+            'form': form,
+        }
+        return render(request, 'blog/add_post.html', context)
