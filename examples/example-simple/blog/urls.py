ce0b6b1395b375a797fe9af001d2b3724e49d87f
Benoit Brayer <brayer.benoit@gmail.com>
2016-03-29 11:56:22 +0200

    Configure debug_toolbar

--- examples/example-simple/blog/urls.py
+++ examples/example-simple/blog/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, url
 from .views import index
 
-urlpatterns = (
-  patterns(
-    '',
-    url(r'^$', index, name='index'),
-  )
-)
+urlpatterns = [
+  url(r'^$', index, name='index'),
+]
