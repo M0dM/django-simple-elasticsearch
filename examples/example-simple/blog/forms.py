d8c1ebef92838da7cf84148c61ae98993a591c3e
Benoit Brayer <brayer.benoit@gmail.com>
2016-03-29 13:27:06 +0200

    - Change to http://knacss.com/ as clean css style - Adding submit buttons to forms (add_blog/add_post) - fix form class to ModelForm

--- examples/example-simple/blog/forms.py
+++ examples/example-simple/blog/forms.py
@@ -2,13 +2,15 @@ from django import forms
 from .models import Blog, BlogPost
 
 
-class BlogForm(forms.Form):
+class BlogForm(forms.ModelForm):
 
     class Meta:
         model = Blog
+        fields = ['name', 'description']
 
 
-class BlogPostForm(forms.Form):
+class BlogPostForm(forms.ModelForm):
 
     class Meta:
         model = BlogPost
+        fields = ['blog', 'title', 'body']
