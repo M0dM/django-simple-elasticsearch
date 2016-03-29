7b8dc00447df31aeb9e51144385f34577aaf7fff
Benoit Brayer <brayer.benoit@gmail.com>
2016-03-29 16:25:20 +0200

    adding django-simple-elasticsearch related code

--- examples/example-simple/blog/models.py
+++ examples/example-simple/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_delete
 from django.template.defaultfilters import slugify
+from simple_elasticsearch.mixins import ElasticsearchIndexMixin
 
 
 class Blog(models.Model):
@@ -10,7 +12,7 @@ class Blog(models.Model):
         return self.name
 
 
-class BlogPost(models.Model):
+class BlogPost(models.Model, ElasticsearchIndexMixin):
     blog = models.ForeignKey(Blog)
     slug = models.SlugField()
     title = models.CharField(max_length=50)
@@ -23,3 +25,67 @@ class BlogPost(models.Model):
             self.slug = slugify(self.title)
 
         super(BlogPost, self).save(*args, **kwargs)
+
+    @classmethod
+    def get_queryset(cls):
+        return BlogPost.objects.all().select_related('blog')
+
+    @classmethod
+    def get_index_name(cls):
+        return 'blog'
+
+    @classmethod
+    def get_type_name(cls):
+        return 'posts'
+
+    @classmethod
+    def get_type_mapping(cls):
+        return {
+            "properties": {
+                "created_at": {
+                    "type": "date",
+                    "format": "dateOptionalTime"
+                },
+                "title": {
+                    "type": "string"
+                },
+                "body": {
+                    "type": "string"
+                },
+                "slug": {
+                    "type": "string"
+                },
+                "blog": {
+                    "properties": {
+                        "id": {
+                            "type": "long"
+                        },
+                        "name": {
+                            "type": "string"
+                        },
+                        "description": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        }
+
+    @classmethod
+    def get_document(cls, obj):
+        return {
+            'created_at': obj.created_at,
+            'title': obj.title,
+            'body': obj.body,
+            'slug': obj.slug,
+            'blog': {
+                'id': obj.blog.id,
+                'name': obj.blog.name,
+                'description': obj.blog.description,
+            }
+        }
+
+# Update / Delete document from index
+# when correponding instance is update / delete from RDBS
+post_save.connect(BlogPost.save_handler, sender=BlogPost)
+pre_delete.connect(BlogPost.delete_handler, sender=BlogPost)
--- examples/example-simple/website/settings.py
+++ examples/example-simple/website/settings.py
@@ -130,3 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Elasticsearch
+# https://github.com/jaddison/django-simple-elasticsearch
+
+ELASTICSEARCH_TYPE_CLASSES = [
+    'blog.models.BlogPost'
+]
