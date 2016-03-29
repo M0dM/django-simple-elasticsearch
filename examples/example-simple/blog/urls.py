880582f71a611b72243bdef8ea1233d532c25499
Benoit Brayer <brayer.benoit@gmail.com>
2016-03-29 19:53:38 +0200

    remove_blog & remove_post added

--- examples/example-simple/blog/urls.py
+++ examples/example-simple/blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import index, add_blog, add_post
+from .views import index, add_blog, add_post, remove_blog, remove_post
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^blog/add$', add_blog, name='add_blog'),
     url(r'^post/add$', add_post, name='add_post'),
+    url(r'^blog/(?P<_id>[0-9]*)/remove$', remove_blog, name='remove_blog'),
+    url(r'^post/(?P<_id>[0-9]*)/remove$', remove_post, name='remove_post'),
 ]
--- examples/example-simple/blog/views.py
+++ examples/example-simple/blog/views.py
@@ -2,12 +2,14 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.utils.translation import ugettext_lazy as _
 from .forms import BlogForm, BlogPostForm
-from .models import BlogPost
+from .models import Blog, BlogPost
 
 
 def index(request):
+    blogs = Blog.objects.all()
     posts = BlogPost.objects.all()
     context = {
+        'blogs': blogs,
         'posts': posts,
     }
     return render(request, 'blog/index.html', context)
@@ -45,3 +47,25 @@ def add_post(request):
             'form': form,
         }
         return render(request, 'blog/add_post.html', context)
+
+
+def remove_blog(request, _id):
+    blog = Blog.objects.filter(pk=_id).get()
+    blog.delete()
+    messages.add_message(
+        request,
+        messages.SUCCESS,
+        _('SUCCESS, blog removed.')
+    )
+    return redirect('blog:index')
+
+
+def remove_post(request, _id):
+    post = BlogPost.objects.filter(pk=_id).get()
+    post.delete()
+    messages.add_message(
+        request,
+        messages.SUCCESS,
+        _('SUCCESS, post removed.')
+    )
+    return redirect('blog:index')
